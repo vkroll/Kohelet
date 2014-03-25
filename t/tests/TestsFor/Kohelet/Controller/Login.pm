359d003a472467c070f5d4fa5f6d8f876f2153d3
Volker Kroll (vkroll) <kroll@strato-rz.de>
2014-03-25 11:53:38 +0100

    using the new Roles Actions and Objects
    * Test no longer needs Catalyst::Test
    * Test uses get_controller from Role Objects
    * Test uses controller_has_action from Role Actions

--- t/tests/TestsFor/Kohelet/Controller/Login.pm
+++ t/tests/TestsFor/Kohelet/Controller/Login.pm
@@ -1,29 +1,16 @@
 package TestsFor::Controller::Login;
 
 use Test::Class::Moose;
-use Catalyst::Test qw( Kohelet );
+with "Actions", "Objects";
+
 use Data::Dumper;
 
 sub test_object {
-    my $o = get_object();
+    my $o = get_controller("Login");
     isa_ok($o, "Kohelet::Controller::Login");
     isa_ok($o, "Catalyst::Controller");
     ok(controller_has_action($o, "index"), "Controller has Action index");
     ok(! controller_has_action($o, "index1"), "Controller has no Action index1");
-}
-
-sub controller_has_action {
-    my($controller, $actionname) = @_;
-    my @actions =  $controller->get_action_methods();
-    @actions = map $_->name, @actions;
-    my @true = grep( /$actionname/, @actions );
-    return defined $true[0];
-}
-
-sub get_object {
-    my($res, $c) = ctx_request( '/');
-    return $c->controller('Login');
-
 }
 
 
